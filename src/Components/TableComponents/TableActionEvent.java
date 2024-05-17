62a01b2e003bc1d1201f1d377529a0c42cc87a5c
YbanezTristan <117066311+normalpersonPC@users.noreply.github.com>
2024-05-18 00:02:54 +0800

    added components for experiencePanel table

---
+++ src/Components/TableComponents/TableActionEvent.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Components.TableComponents;
+
+/**
+ *
+ * @author Admin
+ */
+public interface TableActionEvent {
+    public void onDelete(int row);
+}
